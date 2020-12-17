1a7ed5957ea2083c9a98974197ebda3a82de692b
Maksim Zhashkevych <zhashkevychmaksim@gmail.com>
2020-12-17 15:09:07 +0200

    updated dump filename format to use unix time

--- pkg/file.go
+++ pkg/file.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const filenameTemplate = "%s.dump_%s.sql"
+const filenameTemplate = "%s.dump_%d.sql"
 
 // DumpFile is used to create temporary dump files
 type DumpFile struct {
@@ -55,5 +55,6 @@ func (f *DumpFile) Remove() error {
 }
 
 func generateFilename(prefix string) string {
-	return fmt.Sprintf(filenameTemplate, prefix, time.Now().Format(time.RFC3339))
+	//return fmt.Sprintf(filenameTemplate, prefix, time.Now().Format(time.RFC3339))
+	return fmt.Sprintf(filenameTemplate, prefix, time.Now().Unix())
 }
